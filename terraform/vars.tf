d0a145278a33ebbf2543bf568b5c15ed23a072d8
Ronnald R Machado <rrmachado@id.uff.br>
2021-01-23 18:46:01 -0300

    Terraform files formated with terraform fmt -check

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,7 +4,7 @@ variable "prefix" {
 }
 
 variable "location" {
-  description = "The Azure Region in which all resources will be created"
+  description = "The Azure  Region in which all resources will be created"
   default     = "Brazil South"
 
 }
