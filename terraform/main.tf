61a0386d6f6d40b4fddb881d7ba92df91d3f2cbb
Ronnald R Machado <rrmachado@id.uff.br>
2021-01-24 16:26:44 -0300

    Get a fix resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,15 +18,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-resources"
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
 }
 
 resource "azurerm_app_service_plan" "plan" {
   name                = "${var.prefix}-appserviceplan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   kind                = "Linux"
   reserved            = true
   sku {
@@ -38,9 +37,9 @@ resource "azurerm_app_service_plan" "plan" {
 
 resource "azurerm_app_service" "app" {
   name                = "${var.prefix}-app-service"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = data.azurerm_app_service_plan.plan.id
 
   site_config {
     linux_fx_version    = "PYTHON|3.7"
