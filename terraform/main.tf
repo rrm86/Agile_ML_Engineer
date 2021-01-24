b8bf45c565296120d43c7cd2de45a54c9fe2fe0c
Ronnald R Machado <rrmachado@id.uff.br>
2021-01-24 16:38:08 -0300

    Typo fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_app_service" "app" {
   name                = "${var.prefix}-app-service"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = data.azurerm_app_service_plan.plan.id
+  app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
     linux_fx_version    = "PYTHON|3.7"
